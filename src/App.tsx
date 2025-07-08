43a76f5185759777f2d7530ffebb121a5da166d7
Precious Adedokun <apreezofficial@gmail.com>
2025-07-08 17:20:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,20 @@ import BackgroundDots from './components/BackgroundDots';
 function App() {
   return (
     <div className="relative min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
-      <BackgroundDots />
-      <Header />
-
-      <main className="pt-20 relative z-10">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
+      <div className="fixed inset-0 -z-50">
+        <BackgroundDots />
+      </div>
+      <div className="relative z-10">
+        <Header />
+        
+        <main className="pt-20">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+      </div>
     </div>
   );
 }
