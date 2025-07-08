ebff9a9d36742820d3f5fb0fb98b2ec07a458e8e
Precious Adedokun <apreezofficial@gmail.com>
2025-07-08 18:16:23 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import SuggestionButtons from '../components/SuggestionButtons';
 import InputBox from '../components/InputBox';
 
