92efeabcdf150c6d919c91f102fb70f254bf30d6
Precious Adedokun <apreezofficial@gmail.com>
2025-07-08 17:10:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/Header';
+import BackgroundDots from './components/BackgroundDots';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="relative min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+      <BackgroundDots />
+      <Header />
+
+      <main className="pt-20 relative z-10">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </div>
   );
 }
 
